a1b18a34b0c66b200a7bbc8ff731f9d72e282627
Raymond Penners <raymond.penners@intenct.nl>
2018-03-04 20:11:02 +0100

    chore: Release 0.2.0

--- healthpoint/__init__.py
+++ healthpoint/__init__.py
@@ -4,7 +4,7 @@
 |  | |___ /~~\ |___  |  |  | |    \__/ | | \|  |
 
 """
-VERSION = (0, 1, 0, 'final', 0)
+VERSION = (0, 2, 0, 'final', 0)
 
 __title__ = 'django-healthpoint'
 __version_info__ = VERSION
@@ -12,4 +12,4 @@ __version__ = '.'.join(map(str, VERSION[:3])) + ('-{}{}'.format(
     VERSION[3], VERSION[4] or '') if VERSION[3] != 'final' else '')
 __author__ = 'Raymond Penners'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2017 Raymond Penners and contributors'
+__copyright__ = 'Copyright 2018 Raymond Penners and contributors'
