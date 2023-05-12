1530717837eecbf751d1bbe3e900e73c70dad7eb
Niek van der Linden <niek.vanderlinden@sendcloud.com>
2023-05-12 14:32:46 +0200

    fix(django): fix url deprecation warning and support django >= 3.2
    Fixes deprecation warning thrown for url. Also set minimal django version to 3.2 and allow up to django 4.2.
    Set minimal python version to 3.8.
    
    Closes https://github.com/pennersr/django-healthpoint/issues/4

--- healthpoint/urls.py
+++ healthpoint/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from healthpoint import views
 
 
-urlpatterns = [url(r"^health/$", views.health, name="healthpoint_health")]
+urlpatterns = [re_path(r"^health/$", views.health, name="healthpoint_health")]
